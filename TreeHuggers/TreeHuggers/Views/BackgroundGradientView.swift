8a7d023471a0151de7661d893e970ef60c0803ee
Nigel Krajewski <triplemonkeystudio@gmail.com>
2024-02-06 14:37:28 -0500

    Update BackgroundGradientView.swift

--- TreeHuggers/TreeHuggers/Views/BackgroundGradientView.swift
+++ TreeHuggers/TreeHuggers/Views/BackgroundGradientView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct BackgroundGradientView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LinearGradient(
+            gradient: Gradient(colors: [.lightBlueGradient, .midBlueGradient, .darkBlueGradient, .darkBlueGradient, .midBlueGradient, .lightBlueGradient]),
+            startPoint: .topTrailing,
+            endPoint: .bottomLeading)
+        .ignoresSafeArea()
     }
 }
 
